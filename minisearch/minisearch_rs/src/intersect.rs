8e5ef6f31d7d294611c2c5a677e73d70b3867623
Liljack118 <kamilzajaczek4@gmail.com>
2025-10-31 21:20:36 +0100

    add get_next_doc method

--- minisearch/minisearch_rs/src/intersect.rs
+++ minisearch/minisearch_rs/src/intersect.rs
@@ -2,7 +2,7 @@ use crate::index::Posting;
 use crate::tokenizer::TokenizedQuery;
 use crate::trie::Trie;
 use hashbrown::HashMap;
-use std::cmp::{Ord, Ordering};
+use std::cmp::{Ordering, Reverse};
 use std::collections::BinaryHeap;
 use ulid::Ulid;
 
@@ -16,8 +16,8 @@ struct TokenDocPointer {
 pub struct PostingListIntersection<'a> {
     query: TokenizedQuery,
     index: &'a HashMap<String, Vec<Posting>>,
-    docs: Vec<Ulid>,
-    pointers: HashMap<String, BinaryHeap<TokenDocPointer>>,
+    docs: Vec<Vec<TokenDocPointer>>,
+    pointers: HashMap<String, BinaryHeap<Reverse<TokenDocPointer>>>,
 }
 
 impl Ord for TokenDocPointer {
@@ -46,8 +46,8 @@ impl<'a> PostingListIntersection<'a> {
         index: &'a HashMap<String, Vec<Posting>>,
         fuzzy_trie: &Trie,
     ) -> Option<Self> {
-        let mut docs: Vec<Ulid> = vec![];
-        let mut pointers: HashMap<String, BinaryHeap<TokenDocPointer>> = HashMap::new();
+        let mut docs: Vec<Vec<TokenDocPointer>> = vec![];
+        let mut pointers: HashMap<String, BinaryHeap<Reverse<TokenDocPointer>>> = HashMap::new();
 
         for query_token in query.tokens.iter() {
             for (distance, token) in fuzzy_trie.search(query_token.fuzz, &query_token.text) {
@@ -67,12 +67,16 @@ impl<'a> PostingListIntersection<'a> {
                 pointers
                     .entry_ref(query_token.text.as_str())
                     .or_default()
-                    .push(pointer);
+                    .push(Reverse(pointer));
             }
 
             if !pointers.contains_key(&query_token.text) {
                 return None;
             }
+
+            let (_, doc) =
+                Self::next_doc_index(index, pointers.get_mut(&query_token.text).unwrap());
+            docs.push(doc);
         }
 
         Some(Self {
@@ -81,5 +85,33 @@ impl<'a> PostingListIntersection<'a> {
             docs: docs,
             pointers: pointers,
         })
+    }
+
+    fn next_doc_index(
+        index: &HashMap<String, Vec<Posting>>,
+        pointer: &mut BinaryHeap<Reverse<TokenDocPointer>>,
+    ) -> (f64, Vec<TokenDocPointer>) {
+        let (mut max_score, mut doc_ids) = (0 as f64, Vec::<TokenDocPointer>::new());
+
+        while !pointer.is_empty() && (doc_ids.is_empty() || doc_ids[0] == pointer.peek().unwrap().0)
+        {
+            let p = pointer.pop().unwrap();
+            if p.0.doc_idx + 1 <= index.get(&p.0.token).unwrap().len() as u32 - 1 {
+                pointer.push(Reverse(TokenDocPointer {
+                    doc_id: index.get(&p.0.token).unwrap()[p.0.doc_idx as usize + 1].doc_id,
+                    doc_idx: p.0.doc_idx + 1,
+                    token: p.0.token.clone(),
+                    distance: p.0.distance,
+                }))
+            }
+
+            max_score = f64::max(
+                max_score,
+                index.get(&p.0.token).unwrap()[p.0.doc_idx as usize + 1].score,
+            );
+            doc_ids.push(p.0);
+        }
+
+        return (max_score, doc_ids);
     }
 }
