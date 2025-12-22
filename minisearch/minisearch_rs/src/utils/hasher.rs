9fd3c4d08523a051522a96df4d26d16290f09c8e
Liljack118 <kamilzajaczek4@gmail.com>
2025-12-22 22:45:11 +0100

    persist hasher data

--- minisearch/minisearch_rs/src/utils/hasher.rs
+++ minisearch/minisearch_rs/src/utils/hasher.rs
@@ -1,60 +1,127 @@
-use hashbrown::HashMap;
+use std::{
+    collections::hash_map::Keys,
+    fs::{self, File},
+    io,
+    path::PathBuf,
+};
 
-pub struct TokenHasher {
+use bincode::{Decode, Encode};
+use std::collections::HashMap;
+
+const OPERATIONS_THRESHOLD: u32 = 100_000;
+
+#[derive(Decode, Encode, PartialEq, Debug, Clone)]
+struct TokensStore {
     map: HashMap<String, u32>,
     tokens: Vec<Option<String>>,
     deleted: Vec<u32>,
 }
 
-impl TokenHasher {
-    pub fn new() -> Self {
+impl TokensStore {
+    fn new(map: HashMap<String, u32>, tokens: Vec<Option<String>>, deleted: Vec<u32>) -> Self {
         Self {
-            map: HashMap::new(),
-            tokens: Vec::new(),
-            deleted: Vec::new(),
+            map: map,
+            tokens: tokens,
+            deleted: deleted,
         }
     }
 
-    pub fn add(&mut self, token: String) -> u32 {
-        if let Some(idx) = self.map.get(&token) {
-            return *idx;
+    fn load(path: &PathBuf) -> Result<Self, io::Error> {
+        if !fs::exists(path)? {
+            File::create(path)?;
+            return Ok(Self::new(HashMap::new(), Vec::new(), Vec::new()));
         }
 
-        let idx = if !self.deleted.is_empty() {
-            let idx = self.deleted.pop().unwrap();
-            self.tokens[idx as usize] = Some(token.clone());
+        let mut file = File::open(path)?;
+        match bincode::decode_from_std_read(&mut file, bincode::config::standard()) {
+            Ok(store) => Ok(store),
+            Err(e) => {
+                println!("Warning tokens decode error: {e}");
+                Ok(Self::new(HashMap::new(), Vec::new(), Vec::new()))
+            }
+        }
+    }
+}
+
+pub struct TokenHasher {
+    path: PathBuf,
+    operations: u32,
+    tokens_store: TokensStore,
+}
+
+impl TokenHasher {
+    pub fn load(dir: &PathBuf) -> Result<Self, io::Error> {
+        let path = dir.join("index").join("tokens");
+        Ok(Self {
+            tokens_store: TokensStore::load(&path)?,
+            path: path,
+            operations: 0,
+        })
+    }
+
+    pub fn tokens(&self) -> Keys<'_, String, u32> {
+        self.tokens_store.map.keys()
+    }
+
+    pub fn add(&mut self, token: String) -> Result<u32, io::Error> {
+        if let Some(idx) = self.tokens_store.map.get(&token) {
+            return Ok(*idx);
+        }
+
+        let idx = if !self.tokens_store.deleted.is_empty() {
+            let idx = self.tokens_store.deleted.pop().unwrap();
+            self.tokens_store.tokens[idx as usize] = Some(token.clone());
             idx
         } else {
-            self.tokens.push(Some(token.clone()));
-            self.tokens.len() as u32
+            self.tokens_store.tokens.push(Some(token.clone()));
+            self.tokens_store.tokens.len() as u32
         };
 
-        self.map.insert(token, idx);
-        return idx;
+        self.tokens_store.map.insert(token, idx);
+        self.operations += 1;
+        if self.operations >= OPERATIONS_THRESHOLD {
+            self.operations = 0;
+            self.flush()?;
+        }
+        return Ok(idx);
+    }
+
+    pub fn delete(&mut self, token: u32) -> Result<Option<String>, io::Error> {
+        if token as usize >= self.tokens_store.tokens.len()
+            || self.tokens_store.tokens[token as usize].is_none()
+        {
+            return Ok(None);
+        }
+
+        let token_str = self.tokens_store.tokens[token as usize].take().unwrap();
+        self.tokens_store.deleted.push(token);
+        self.tokens_store.map.remove(&token_str);
+        self.operations += 1;
+        if self.operations >= OPERATIONS_THRESHOLD {
+            self.operations = 0;
+            self.flush()?;
+        }
+        Ok(Some(token_str))
     }
 
     pub fn hash(&self, token: &String) -> Option<u32> {
-        match self.map.get(token) {
+        match self.tokens_store.map.get(token) {
             Some(idx) => Some(*idx),
             None => None,
         }
     }
 
     pub fn unhash(&self, token: u32) -> Option<&String> {
-        match self.tokens.get(token as usize) {
+        match self.tokens_store.tokens.get(token as usize) {
             Some(val) => val.as_ref(),
             None => None,
         }
     }
 
-    pub fn delete(&mut self, token: u32) -> Option<String> {
-        if token as usize >= self.tokens.len() || self.tokens[token as usize].is_none() {
-            return None;
-        }
-
-        let token_str = self.tokens[token as usize].take().unwrap();
-        self.deleted.push(token);
-        self.map.remove(&token_str);
-        Some(token_str)
+    pub fn flush(&self) -> Result<(), io::Error> {
+        let mut file = File::create(&self.path)?;
+        bincode::encode_into_std_write(&self.tokens_store, &mut file, bincode::config::standard())
+            .unwrap();
+        Ok(())
     }
 }
